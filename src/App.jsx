f5ac3b06ea0c13999eb9eef07cfb0382fbf8814b
Daniil Kuznetsov <daniil.r1dy@ya.ru>
2025-05-04 22:43:13 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-let datenow = new Date()
+let datenow = new Date();
+
+const products = [
+  { title: "Капуста", isFruit: false, id: 1 },
+  { title: "Чеснок", isFruit: false, id: 2 },
+  { title: "Яблоко", isFruit: true, id: 3 },
+];
+
+const listItems = products.map((product) => (
+  <li
+    key={product.id}
+    style={{ color: product.isFruit ? "magneta" : "darkgreen" }}>
+    {product.title}
+  </li>
+));
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [time, setTime] = useState(datenow.toTimeString().substring(0, 8))
-  
-  function getDate() {
-    let datenow = new Date()
-    setTime(datenow.toTimeString().substring(0, 8))
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
   }
-  
-  setInterval(getDate, 1000)
+
+  function MyButton(props) {
+    return <button onClick={props.onClick}>Кликнули {props.count} раз</button>;
+  }
+
+  function MyUser() {
+    return (
+      <>
+        <h2>{user.name}</h2>
+        <img className="avatar" src={user.imageUrl} />
+      </>
+    );
+  }
 
   return (
     <>
+      <MyUser />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -26,11 +58,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{time}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <ul>{listItems}</ul>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -39,7 +68,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
